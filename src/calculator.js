d994c62ab595cc1f48f584324e4a4d64ee608ef2
Ukaviya <kaviyaumayun@gmail.com>
2024-07-19 21:07:43 +0530

    returns sum when 2 numbers are given

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,7 @@ function add(numbers){
    if(numbers === ""){
     return 0;
    } 
-   return parseInt(numbers);
+   const numArray=numbers.split(',');
+   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 module.exports=add;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,3 +5,6 @@ test('for empty string return 0',()=>{
 test ('for single number return 1',()=>{
     expect(add("1")).toBe(1);
 });
+test('for two numbers retur sum of them',()=>{
+    expect(add("1,2")).toBe(3);
+});
