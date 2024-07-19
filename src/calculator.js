9e85ba079276aba26fa9a3fcb1b3abb98048ea41
Ukaviya <kaviyaumayun@gmail.com>
2024-07-19 22:01:38 +0530

    throws an error for negative numbers

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,13 @@ function add(numbers){
    if(numbers === ""){
     return 0;
    } 
-   const numArray=numbers.split(/[\n,]/);
-   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+   const numArray = numbers.split(/[\n,]/);
+   const negatives = numArray.filter(num => num < 0);
+   if (negatives.length > 0) {
+       throw new Error("negative numbers are not allowed");
+   }
+
+   return numArray.reduce((sum, num) => sum + (+num), 0);
 }
+
 module.exports=add;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,6 +5,9 @@ test('for empty string return 0',()=>{
 test ('for single number return 1',()=>{
     expect(add("1")).toBe(1);
 });
-test('for two numbers retur sum of them',()=>{
+test('for two numbers return sum of them',()=>{
     expect(add("1\n2,3")).toBe(6);
+});
+test('throws error when they are negative numbers ' ,()=>{
+    expect(()=>add("-1,-2,9")).toThrow("negative numbers are not allowed")
 });
