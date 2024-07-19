f7d7157727441b055a1c855f7b5967a163629b22
Ukaviya <kaviyaumayun@gmail.com>
2024-07-19 20:50:06 +0530

    for single number return 1

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,3 +2,6 @@ const add = require('../src/calculator');
 test('for empty string return 0',()=>{
     expect(add("")).toBe(0);
 });
+test ('for single number return 1',()=>{
+    expect(add("1")).toBe(1);
+});
